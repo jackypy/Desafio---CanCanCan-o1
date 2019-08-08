29e7c1a475993a3a289b38b7c0aee2c461f3f574
jackypy <jacqueline.parra7@gmail.com>
2019-08-07 20:36:07 -0400

    unico

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def dashboard
+  @user = User.all
+  end
+
+  def become_admin
+    @user = User.find(params[:format])
+    @user.admin!
+    redirect_to pages_dashboard_path
+  end
+
+  def become_visit
+    @user = User.find(params[:format])
+    @user.visit!
+    redirect_to pages_dashboard_path
+  end
+
+  def delete_user
+    @user = User.find(params[:format])
+    @user.destroy!
+    redirect_to pages_dashboard_path
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  get 'pages/dashboard'
+  get 'pages/become_admin'
+  get 'pages/become_visit'
+  delete 'pages/delete_user'
+
+	resources :posts do
+		resources :comments, only: [:create, :destroy]
+	end
+
+	devise_for :users, controllers: {
+		sessions: 'users/sessions',
+		registrations: 'users/registrations'
+	}
+
+	root 'posts#index'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
